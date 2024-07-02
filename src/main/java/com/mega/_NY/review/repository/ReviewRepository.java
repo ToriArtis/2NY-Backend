5abb7b86ca3fbae083b08493910775a24da5ab7a
unknown <aralshyun@naver.com>
2024-07-02 17:39:54 +0900

    ✨ Pagenation Add
    Pagenation Add

--- src/main/java/com/mega/_NY/review/repository/ReviewRepository.java
+++ src/main/java/com/mega/_NY/review/repository/ReviewRepository.java
@@ -1,13 +1,13 @@
 package com.mega._NY.review.repository;
 
 import com.mega._NY.review.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-    List<Review> findByItemItemId(Long itemId);
-    List<Review> findByUserId(Long userId);
+    Page<Review> findByItemItemId(Long itemId, Pageable pageable);
+    Page<Review> findByUserId(Long userId, Pageable pageable);
 }
