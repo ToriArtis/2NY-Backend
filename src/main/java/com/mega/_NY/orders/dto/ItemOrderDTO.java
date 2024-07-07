fb4d79818a4c2d396f4f9d6e75a9e903525b0c39
SiNiKin <tlsdl99@gmail.com>
2024-07-07 15:41:47 +0900

    🧵 Chore: ItemOrdersDTO in ItemSize + Color
    Chore: ItemOrdersDTO in ItemSize + Color

--- src/main/java/com/mega/_NY/orders/dto/ItemOrderDTO.java
+++ src/main/java/com/mega/_NY/orders/dto/ItemOrderDTO.java
@@ -1,5 +1,10 @@
 package com.mega._NY.orders.dto;
 
+import com.mega._NY.item.entity.ItemColor;
+import com.mega._NY.item.entity.ItemSize;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.Min;
 import lombok.*;
 
@@ -19,5 +24,9 @@ public class ItemOrderDTO {
     private int quantity;     // 주문 수량
     private int price;        // 개별 아이템 가격
     private int totalPrice;   // 수량 * 가격
+
+    private ItemSize size;
+    private ItemColor color;
+
 
 }
